4bde1923fc6cd18de7495975e23d565b8592269e
Louison <louison.boisselier@gmail.com>
2020-11-24 12:46:59 +0100

    add lineitems

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true
   validates :image_url, presence: true
-
+  has_many :lineitems
 
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   validates_presence_of :first_name, :last_name
 
-  has_many  :cart
+  has_many :carts
 end
