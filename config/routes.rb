cfeb1d83bb2c82318c746dfa4a0fbb059a9e0192
Louison <louison.boisselier@gmail.com>
2020-11-24 17:56:38 +0100

    add back cart

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'static_pages/secret'
   devise_for :users
   resources :items
-  resources :line_items
+  resources :lineitems
   
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
